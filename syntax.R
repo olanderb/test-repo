8dd1960620df5cf5f6e8e1c34037d0117b5b58ce
OLANDER William <william.olander@wfp.org>
2018-03-02 09:58:27 +0000

    after guinea dom worked on it

--- syntax.R
+++ syntax.R
@@ -6,13 +6,13 @@ library(ggplot2)
 #read in dataset using relative path
 GIN <- read_excel("/test-repo/201801_GIN_EFSAdonneesbrutes.xlsx")
 
-#trying how the anonymize function on "Operator" variable but it just creates values, how do i get it to replace the column Operator?
-thingy <- anonymize(GIN$Operator, .algo = "sha256")
+#use the anonymize function on "Interviewer Number" variable but it just creates values, how do i get it to replace the column Interveiwer Number?
+thingy <- anonymize(GIN$`Interviewer Number`, .algo = "sha256")
 
 #attaching the anonymized number to the dataset
 GIN <- GIN %>% mutate(anonymizednumber = thingy)
 
 
-#plots of FCS and rCSI
+#plots of number of members in household by sex of respondent
 p10 <- ggplot(GIN, aes(x = sex, y = Nbre_membre)) + geom_boxplot()
 print(p10)
