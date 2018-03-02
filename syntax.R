6138677bac2528e12324b04409b93be7371e98f7
OLANDER William <william.olander@wfp.org>
2018-03-02 08:56:39 +0000

    after i add something

--- syntax.R
+++ syntax.R
@@ -5,9 +5,11 @@ library(anonymizer)
 #read in the data set - how do i make this a relative path?
 GIN <- read_excel("C:/Users/Catharine/Desktop/VAMSPSSTEST_201801_GIN_EFSA/1_Données Brutes/201801_GIN_EFSAdonneesbrutes.xlsx")
 GIN2 <- GIN
+GIN3 <- GIN
 
 #trying how the anonymize function on "Operator" variable but it just creates values, how do i get it to replace the column Operator?
 thingy <- anonymize(GIN$Operator, .algo = "sha256")
 
 #trying some
 GIN <- GIN %>% mutate(anonymizednumber = thingy)
+
