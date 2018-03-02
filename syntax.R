cfa333ad8c097c44d2acf28e97fee80b91c0c041
Ahmadou Dicko <ahmadou.dicko@gmail.com>
2018-03-02 15:20:09 +0000

    Update code (relative path and use mutate i.e tidy code)

--- syntax.R
+++ syntax.R
@@ -4,15 +4,20 @@ library(anonymizer)
 library(ggplot2)
 
 #read in dataset using relative path
-GIN <- read_excel("/test-repo/201801_GIN_EFSAdonneesbrutes.xlsx")
+GIN <- read_excel("./201801_GIN_EFSAdonneesbrutes.xlsx")
 
-#use the anonymize function on "Interviewer Number" variable but it just creates values, how do i get it to replace the column Interveiwer Number?
-thingy <- anonymize(GIN$`Interviewer Number`, .algo = "sha256")
+## Check data type
+glimpse(GIN)
 
-#attaching the anonymized number to the dataset
-GIN <- GIN %>% mutate(anonymizednumber = thingy)
 
+## use the anonymize function on "Interviewer Number" variable but it just creates values, how do i get it to replace the column Interveiwer Number?
+GIN <- mutate(GIN, anonymizednumber = anonymize(`Interviewer Number`, .algo = "sha256"))
+glimpse(GIN)
+
+## Check the data
+View(select(GIN, anonymizednumber, `Interviewer Number`))
 
 #plots of number of members in household by sex of respondent
-p10 <- ggplot(GIN, aes(x = sex, y = Nbre_membre)) + geom_boxplot()
+p10 <- ggplot(GIN, aes(x = sex, y = Nbre_membre)) +
+  geom_boxplot()
 print(p10)
