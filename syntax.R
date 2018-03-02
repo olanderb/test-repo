ecf4fd54c45a5b08e3f12c9612a4613cd1868341
OLANDER William <william.olander@wfp.org>
2018-03-02 09:33:15 +0000

    changed import to relative path

--- syntax.R
+++ syntax.R
@@ -3,14 +3,13 @@ library(dplyr)
 library(anonymizer)
 library(ggplot2)
 
-#read in the data set - how do i make this a relative path?
-GIN <- read_excel("C:/test-repo/201801_GIN_EFSAdonneesbrutes.xlsx")
-
+#read in dataset using relative path
+GIN <- read_excel("/test-repo/201801_GIN_EFSAdonneesbrutes.xlsx")
 
 #trying how the anonymize function on "Operator" variable but it just creates values, how do i get it to replace the column Operator?
 thingy <- anonymize(GIN$Operator, .algo = "sha256")
 
-#trying some
+#attaching the anonymized number to the dataset
 GIN <- GIN %>% mutate(anonymizednumber = thingy)
 
 
